49542250ce98e00f476f307884acd75409ab41f0
Cindy Cicalese <cindom@gmail.com>
2026-02-01 17:13:10 -0500

    Add LocalSettings.php template in config/settings/global/

---
+++ config/settings/global/LocalSettings.php
@@ -0,0 +1,22 @@
+<?php
+# =============================================================================
+# Global Settings (LocalSettings.php)
+# =============================================================================
+# This file contains settings that apply to ALL wikis in the farm.
+# It is loaded before per-wiki settings, so per-wiki files can override these.
+#
+# Examples of what you can add here:
+#   - wfLoadExtension( 'VisualEditor' );
+#   - wfLoadSkin( 'Vector' );
+#   - $wgLanguageCode = "en";
+#   - $wgDefaultSkin = "vector-2022";
+#   - $wgGroupPermissions['sysop']['interwiki'] = true;
+#
+# For per-wiki settings, edit config/settings/wikis/<wiki_id>/Settings.php
+# =============================================================================
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	exit;
+}
+
+# Add global customizations below:
