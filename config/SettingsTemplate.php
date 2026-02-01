06d997f705be10f4c553a28e1f08caa88497c2c6
Cindy Cicalese <cindom@gmail.com>
2026-02-01 10:03:32 -0500

    Clean up SettingsTemplate.php to remove misleading examples
    - Remove example settings that are already set by CanastaDefaultSettings.php
    - Add clear documentation about which variables are set automatically
    - Remove confusing comments about values that will be overwritten
    
    Fixes #76

--- config/SettingsTemplate.php
+++ config/SettingsTemplate.php
@@ -1,8 +1,24 @@
 <?php
-// If not running MediaWiki, exit
+# =============================================================================
+# Per-Wiki Settings for: {WIKI_ID}
+# =============================================================================
+# This file contains settings specific to this wiki.
+# It is loaded after global settings and can override them.
+#
+# DO NOT set these variables here (they are set automatically from wikis.yaml):
+#   - $wgDBname
+#   - $wgServer
+#   - $wgSitename
+#   - $wgMetaNamespace
+#
+# Examples of what you CAN set here:
+#   - $wgLanguageCode = "es";
+#   - $wgLogo = "/path/to/wiki-specific-logo.png";
+#   - wfLoadExtension( 'WikiSpecificExtension' );
+# =============================================================================
+
 if ( !defined( 'MEDIAWIKI' ) ) {
 	exit;
 }
-#$wgServer = "https://localhost";
-#$wgSitename = ;
-#$wgMetaNamespace = ;
+
+# Add per-wiki customizations below:
