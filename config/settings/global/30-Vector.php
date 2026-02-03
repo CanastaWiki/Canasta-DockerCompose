bb3965264c2a92bcbfa998319b76890963a714cd
Cindy Cicalese <cindom@gmail.com>
2026-02-03 02:30:54 -0500

    Add default skin setting to 30-Vector.php
    Move $wgDefaultSkin from CanastaDefaultSettings.php to this
    user-configurable file so it can be easily changed or removed.

--- config/settings/global/30-Vector.php
+++ config/settings/global/30-Vector.php
@@ -1,4 +1,6 @@
 <?php
-// Load the Vector skin - this is a default Canasta file
-// To not have Vector installed, simply delete this file.
+// Load the Vector skin and set it as the default.
+// This is a default Canasta file. To not have Vector installed,
+// simply delete this file.
+$wgDefaultSkin = "vector-2022";
 wfLoadSkin( 'Vector' );
