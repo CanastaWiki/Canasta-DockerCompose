b6b259a9147dea10930ec6e81d7b108b94897713
Cindy Cicalese <cindom@gmail.com>
2026-02-19 10:48:24 -0500

    Remove shared files now embedded in Canasta-CLI
    Shared installation template files (.env.example, my.cnf,
    config/default.vcl, settings, directory scaffolding) are now
    embedded in the CLI binary using Go's embed.FS. This eliminates
    duplication between orchestrator repos and ensures consistency
    across Docker Compose and Kubernetes installations.
    
    The CLI copies these shared files to the installation directory
    during `canasta create`, after cloning this repo for the
    orchestrator-specific files (docker-compose.yml, etc.).

--- config/settings/global/CanastaFooterIcon.php
+++
@@ -1,8 +0,0 @@
-<?php
-// We appreciate your support for Canasta!
-// If you would like to remove the Canasta logo from your wiki, simply delete this file.
-$wgFooterIcons['poweredby']['canasta'] = [
-    'src' => "$wgResourceBasePath/resources/assets/Powered-by-Canasta.png",
-    'url' => 'https://www.canasta.wiki/',
-    'alt' => 'Powered by Canasta'
-];
--- config/settings/global/Vector.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Load the Vector skin and set it as the default.
-// This is a default Canasta file. To not have Vector installed,
-// simply delete this file.
-$wgDefaultSkin = "vector-2022";
-wfLoadSkin( 'Vector' );
